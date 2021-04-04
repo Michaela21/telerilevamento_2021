0cd1690f4df1b7b91b7d88e468719f4a689be9d6
Michaela21 <80678478+Michaela21@users.noreply.github.com>
2021-04-04 19:34:27 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -2,6 +2,9 @@
 #Greeland increase of temperature
 #Data and code from Emanuela Cosma
 
+#install.packages("ggplot2")
+#install.packages("rasterVis")
+
 #install.pakages("raster")
 library(raster)
 
