638fb0b2247e10c5fb5ae2d753f494670d97e1f4
Michaela21 <80678478+Michaela21@users.noreply.github.com>
2021-04-07 14:32:08 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -8,4 +8,27 @@
 #install.pakages("raster")
 library(raster)
 
-setwd("C:/lab/greeland")
+setwd("C:/lab/greenland")#indicare cartella di lavoro
+
+lst_2000 <- raster("lst_2000.tif")# importo il singolo raster, rinominandolo, in R
+plot(lst_2000)
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+#list of files
+rlist <- list.files(pattern="lst")#crea lista di file e li nomina vcome oggetto
+rlist #per vedere la lista dei file
+
+import <-lapply (rlist,raster) #caricare in R la lista dei file precedentemente creata
+
+TGr <- stack(import)#per creare un unico file di raster 
+
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+plotRGB(TGr, 2, 3, 4, stretch="Lin")
