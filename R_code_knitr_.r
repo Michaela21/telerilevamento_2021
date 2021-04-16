1dc498d44aa7ee461b40126664dfbff811c738bc
Michaela21 <80678478+Michaela21@users.noreply.github.com>
2021-04-16 17:06:55 +0200

    Update R_code_knitr_.r

--- R_code_knitr_.r
+++ R_code_knitr_.r
@@ -4,4 +4,6 @@ setwd("C:/lab/")
 
 library(knitr) #uguale a require(knitr)
 stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
-stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))# riga codice Rocchini
+stitch("C:/lab/R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))# riga codice Rocchini
+stitch("R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("C:/lab/R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
