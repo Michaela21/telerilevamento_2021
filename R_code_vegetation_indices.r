2689dd74c5dd21df6778665bc13dca2fa799813d
Michaela21 <80678478+Michaela21@users.noreply.github.com>
2021-04-30 10:30:44 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,12 +1,57 @@
 #R_code_vegetation_indices.r
-#library(raster)
+library(raster)#si può usare anche require(raster)
+library(RStoolbox)#per usare la funzione spectraIndices al posto di fare l'oporazione algebrica
 
 setwd("C:/lab/")
 
-defor1<-brick(defor1.jpg)
-defor2<-brick(defor2.jpg)
+defor1<-brick("defor1.jpg")
+defor2<-brick("defor2.jpg")
 
 # b1=NIR, b2=red, b3=green
 par(mfrow=c(1,2))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+
+#difference vegetation
+
+dvi1 <- defor1$defor1.1 - defor1$defor1.2#DVI di derfor1
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi1, col=cl, main="DVI at time 1")
+
+dvi2 <- defor2$defor2.1 - defor2$defor2.2#DVI immagine più recente (defor2)
+plot(dvi2, col=cl, main="DVI at time 2")
+
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI at time 1")
+plot(dvi2, col=cl, main="DVI at time 2")
+
+#differnza tra dvi
+difdiv <- dvi1-dvi2
+cld <- colorRampPalette(c('blue','white','red'))(100)
+plot(difdiv, col=cld)
+
+#NDVI (normalized difference vegetation indices)
+# (NIR-Red)/(NIR+Red)
+ndvi1<- (defor1$defor1.1 - defor1$defor1.2)/(defor1$defor1.1 + defor1$defor1.2)# uguale a dvi1/(defor1$defor1.1 + defor1$defor1.2)
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(ndvi1, col=cl)
+
+
+ndvi2<- (defor2$defor2.1 - defor2$defor2.2)/(defor2$defor2.1 + defor2$defor2.2)# uguale a dvi1/(defor1$defor1.1 + defor1$defor1.2)
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(ndvi2, col=cl)
+
+#Nel pacchetto RStoolbox si può usare il pacchetto spectralIndices
+
+vi1 <- spectralIndices(defor1, green = 3, red = 2, nir = 1)
+plot(vi1, col=cl)
+
+vi2 <- spectralIndices(defor1, green = 3, red = 2, nir = 1)
+plot(vi2, col=cl)
+
+#differenza ndvi
+ndifdiv <- ndvi1-ndvi2
+cld <- colorRampPalette(c('blue','white','red'))(100)
+plot(ndifdiv, col=cld)
+
+
